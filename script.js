aaa13ce80bebcac87eb7a3945deab13685b17f14
a-hoffmann <alexandr.hoffmann@gmail.com>
2016-05-16 20:38:25 +0200

    test of a test

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ module.exports = new Script({
             return bot.getProp('name')
                 .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
                         'teach me how to do anything else!'))
-                .then(() => 'finish');
+                .then(() => 'test');
         }
     }
 });
